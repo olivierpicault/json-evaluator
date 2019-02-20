54c7083dc21b9f6067469cf8dd6f2b9083b45589
olivierpicault <olivier.picault@gmail.com>
2019-02-20 17:20:01 +0100

    init

---
+++ readme.md
@@ -0,0 +1,148 @@
+# json-evaluator
+
+The aim of the json is to produce a boolean
+
+## Installation
+
+## Usage
+
+## How does the json work ?
+
+The json needs 3 properties:
+- a value to compare
+- a value to compare to
+- an operator
+
+```
+{
+    "compare": {},
+    "compareTo": {},
+    "operator": ""
+}
+```
+
+A value is defined by two properties:
+- The type of the value
+- The value itself
+
+```
+{
+    "type": "",
+    "value: "",
+}
+```
+
+Obviously:
+- the type and the value have to match
+- the operator has to be one of the allowed as well 
+
+Allowed types are:
+- string
+- number
+- boolean 
+- expression
+- field
+
+An `expression` is simply another node to compare, the value is thus the object you want to evaluate.
+
+A `field` can be used to give an external value. External values can be given through a dictionnary. The value will then be the dictionnary key that holds the value.
+
+Allowed operators are:
+- `>`
+- `<=`
+- `>`
+- `>=`
+- `==`
+- `!=`
+- `||`
+- `&&`
+- `or`
+- `and`
+
+They obviously can't be applied to all types.
+
+## Examples
+
+### Not all operators match
+
+#### Bad
+```
+{
+  "compare": {
+    "type": "string",
+    "value": "olivier"
+},
+{
+  "compareTo": {
+    "type": "string",
+    "value": "arnaud"
+},
+"operator": ">="
+```
+This will produce an error
+
+#### Good
+```
+{
+  "compare": {
+    "type": "string",
+    "value": "olivier"
+},
+{
+  "compareTo": {
+    "type": "string",
+    "value": "arnaud"
+},
+"operator": "=="
+```
+This will produce (bool) false
+
+### Type and value have to match
+
+#### Bad
+```
+{
+  "compare": {
+    "type": "string",
+    "value": 14
+}
+```
+
+#### Good
+```
+{
+  "compare": {
+    "type": "number",
+    "value": 14
+  }
+}
+```
+
+
+### "Expression" type
+```
+{
+  "compare": {
+    "type": "expression",
+    "value": {
+      "compare": {
+        "type": "string",
+        "value": "olivier"
+      },
+      "compareTo": {
+        "type": "string",
+        "value": "arnaud"
+      },
+      "operator": "=="
+    }
+  },
+  "compareTo": {
+    "type": "boolean",
+    "value": true
+  },
+  "operator": "&&"
+}
+````
+This will produce (bool) false because:
+- "olivier" == "arnaud" is false
+- true && false is false
---
+++ tests/evaluate.test.js
@@ -0,0 +1,179 @@
+import evaluate from '../evaluate';
+
+[{
+  instance: {
+    compare: { value: 'hello' },
+    compareTo: { value: 'hello' },
+    operator: '==',
+  },
+  expected: true,
+}, {
+  instance: {
+    compare: { value: 'hello' },
+    compareTo: { value: 'hi' },
+    operator: '==',
+  },
+  expected: false,
+}, {
+  instance: {
+    compare: { value: 'hello' },
+    compareTo: { value: 'hello' },
+    operator: '!=',
+  },
+  expected: false,
+}, {
+  instance: {
+    compare: { value: 'hi' },
+    compareTo: { value: 'hello' },
+    operator: '!=',
+  },
+  expected: true,
+}, {
+  instance: {
+    compare: { value: 0 },
+    compareTo: { value: 1 },
+    operator: '==',
+  },
+  expected: false,
+}, {
+  instance: {
+    compare: { value: 0 },
+    compareTo: { value: 1 },
+    operator: '!=',
+  },
+  expected: true,
+}, {
+  instance: {
+    compare: { value: 0 },
+    compareTo: { value: 1 },
+    operator: '>=',
+  },
+  expected: false,
+}, {
+  instance: {
+    compare: { value: 0 },
+    compareTo: { value: 1 },
+    operator: '<=',
+  },
+  expected: true,
+}, {
+  instance: {
+    compare: { value: 0 },
+    compareTo: { value: 1 },
+    operator: '>',
+  },
+  expected: false,
+}, {
+  instance: {
+    compare: { value: 0 },
+    compareTo: { value: 1 },
+    operator: '<',
+  },
+  expected: true,
+}, {
+  instance: {
+    compare: { value: true },
+    compareTo: { value: true },
+    operator: '==',
+  },
+  expected: true,
+}, {
+  instance: {
+    compare: { value: true },
+    compareTo: { value: false },
+    operator: '==',
+  },
+  expected: false,
+}, {
+  instance: {
+    compare: { value: true },
+    compareTo: { value: true },
+    operator: '&&',
+  },
+  expected: true,
+}, {
+  instance: {
+    compare: { value: true },
+    compareTo: { value: false },
+    operator: 'and',
+  },
+  expected: false,
+}, {
+  instance: {
+    compare: { value: false },
+    compareTo: { value: true },
+    operator: '||',
+  },
+  expected: true,
+}, {
+  instance: {
+    compare: { value: true },
+    compareTo: { value: false },
+    operator: 'or',
+  },
+  expected: true,
+}].forEach((testCase) => {
+  test('Evaluate basic', () => {
+    expect(evaluate(testCase.instance)).toEqual(testCase.expected);
+  });
+});
+
+test('Evaluate with expression', () => {
+  const instance = {
+    compare: {
+      type: 'expression',
+      value: {
+        compare: {
+          type: 'number',
+          value: 0,
+        },
+        compareTo: {
+          type: 'number',
+          value: 1,
+        },
+        operator: '>',
+      },
+    },
+    compareTo: {
+      type: 'boolean',
+      value: true,
+    },
+    operator: '&&',
+  };
+  expect(evaluate(instance)).toEqual(false);
+});
+
+test('Evaluate with expression 2', () => {
+  const instance = {
+    compare: {
+      type: 'expression',
+      value: {
+        compare: {
+          type: 'number',
+          value: 23,
+        },
+        compareTo: {
+          type: 'number',
+          value: 77,
+        },
+        operator: '<=',
+      },
+    },
+    compareTo: {
+      type: 'expression',
+      value: {
+        compare: {
+          type: 'string',
+          value: 'hello',
+        },
+        compareTo: {
+          type: 'string',
+          value: 'hi',
+        },
+        operator: '!=',
+      },
+    },
+    operator: '==',
+  };
+  expect(evaluate(instance)).toEqual(true);
+});
